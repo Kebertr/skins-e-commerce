8cfe78aca75b6e7f2b18e241dababaec71eb301f
Kebert <rasmus.kebert@gmail.com>
2025-02-18 10:06:49 +0100

    Changed react-app to vite with react and made connection to express

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+const mysql = require('mysql2');
 
-app.use((req, res) => {
-    res.status(200).send('Hello, world!');
+const cors = require('cors');
+
+
+app.use(cors());
+
+let connection;
+function makeconnection(){
+  connection = mysql.createConnection({
+    host: 'database', 
+    user: 'user',
+    password: 'password',
+    database: 'mydatabase',
+    port: '3306',
+  });
+  connection.connect();
+}
+
+setTimeout(makeconnection, 3000);
+
+app.get('/api/users', (req, res) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      res.status(500).send('Problem gettings users');
+      return;
+    }
+    res.json(results);
+  });
 });
-// Start the server
+
+
+
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
 });
