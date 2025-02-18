cd8e26f220119350cb56e5afa5cd8ab5660797c8
Kebert <rasmus.kebert@gmail.com>
2025-02-18 20:47:48 +0100

    We can now get database entries displayed on frontend

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,12 @@ function makeconnection(){
   connection.connect();
 }
 
-setTimeout(makeconnection, 3000);
+setTimeout(makeconnection, 5000);
 
 app.get('/api/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) {
-      res.status(500).send('Problem gettings users');
+      alert('Problem gettings users');
       return;
     }
     res.json(results);
@@ -35,8 +35,7 @@ app.get('/api/users', (req, res) => {
 app.get('/api/skins', (req, res) => {
   connection.query('SELECT * FROM skins', (err, results) => {
     if (err) {
-      console.error('Error executing query: ' + err.stack);
-      res.status(500).send('Error fetching users');
+      alert('Problem gettings skins');
       return;
     }
     res.json(results);
@@ -47,7 +46,6 @@ app.post('/api/createskin', (req, res) => {
   const { id, name, category, value, product_id, image_location } = req.body;
   connection.query('INSERT INTO skins (id, name, category, value, product_id, image_location) VALUES (?, ?, ?, ?, ?, ?)', [id, name, category, value, product_id, image_location], (err, result) => {
     if (err) {
-      console.error('Error executing query: ' + err.stack);
       res.status(400).send('Error creating skin');
       return;
     }
