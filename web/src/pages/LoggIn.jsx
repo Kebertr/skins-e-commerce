993c809de115a25f415bb80774ebd7e92b060087
Olof_Werner <olof.p.werner@gmail.com>
2025-02-21 14:27:55 +0100

    logg-in and register page

---
+++ web/src/pages/LoggIn.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import '../styles/LoggInStyles.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+//The whole logg in page
+function LoggIn() {
+    const [formData, setFormData] = useState({username: "", password: ""})
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(formData.username + " " + formData.password)
+        navigate("/");
+    }
+
+    return(
+        <div className="login-container">
+            <h2>Login</h2>
+            <form action="#" method="POST" onSubmit={handleSubmit}>
+                <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
+                <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+                <button type="submit" >Log In</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoggIn;
---
+++ web/src/pages/Register.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import '../styles/LoggInStyles.css';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+//The whole logg in page
+function Register() {
+    const [formData, setFormData] = useState({p1: "", p2: ""})
+    const [passwordText, setPasswordText] = useState("");
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.id]: event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("handleSubmit")
+        navigate("/");
+    }
+
+    useEffect(() => {
+        if (formData.p1 === formData.p2 && formData.p1 !== "") {
+            setPasswordText("✅ Matching");
+        } else if (formData.p1 === "" && formData.p2 === "") {
+            setPasswordText(""); // Clears message when both fields are empty
+        } else {
+            setPasswordText("🚨 Passwords not matching");
+        }
+    }, [formData]);
+
+    return(
+        <div className="login-container">
+            <h2>Register</h2>
+            <form action="#" method="POST" onSubmit={handleSubmit}>
+                <input type="text" name="username" placeholder="Username" required />
+                <input id="p1" type="password" name="password" placeholder="Password" onChange={handleChange} required />
+                <input id="p2" type="password" name="password" placeholder="Reenter Password" onChange={handleChange} required />
+                <p>{passwordText}</p>
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
